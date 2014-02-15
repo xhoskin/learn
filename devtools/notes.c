cd1bd454c8deccb78baa4ff7ef5c6f658385e3dd
xhoskin <xhoskin@gmail.com>
2014-02-15 21:58:18 +0600

    till Styles Sidebar section

---
+++ devtools/notes.c
@@ -0,0 +1,30 @@
+// `Opening DevTools
+Ctrl + Shift + I        //Open Developer Tools	
+Ctrl + Shift + J        //Open Developer Tools with console	
+
+
+// `All Panels
+Show General Settings dialog	  //?, F1
+Next panel	                    //Ctrl + ]
+Previous panel	                //Ctrl + [
+Backward in panel History	      //Ctrl + Alt + [
+Forward in panel history	      //Ctrl + Alt + ]
+Jump to panel 1-9 	            //Ctrl + 1-9 (when enabled in General Settings)
+Jump to elements                //Ctrl + 1
+Jump to console                 //Ctrl + 8
+Refresh 	                      //F5, Ctrl + R
+Refresh clear cache             //Ctrl + F5, Ctrl + Shift + R
+Search by filename	            //Ctrl + O  (except on Timeline)
+Restore default text size	      //Ctrl + 0
+Zoom in	                        //Ctrl + +
+Zoom out	                      //Ctrl + -
+
+
+// `Elements Panel
+H     //Hide element (visibility hidden) 
+
+
+//Styles Sidebar
+Go to line of style rule property declaration in source	Ctrl + Click on property
+Go to line of property value declaration in source	Ctrl + Click on property value
+View auto-complete suggestions	Ctrl + Space
